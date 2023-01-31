3d0ea9e144e2d56f82499d3a88dae1a17778a734
Gideon <gideons564@gmail.com>
2023-01-31 12:41:26 +0100

    Modify the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
 
+  namespace :api do
+    namespace :v1 do
+      resources :items, param: :slug
+      resources:reviews, only: [:create, :destroy]
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '*path', to: 'pages#index', via: :all
 end
